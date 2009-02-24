46c008b2ba31b4b252a3737125ff79e210c89e19
Bryan Helmkamp <bryan@brynary.com>
2009-02-24 00:28:20 -0500

    Working on changing method signatures

--- lib/rack/test.rb
+++ lib/rack/test.rb
@@ -11,63 +11,67 @@ module Rack
       attr_reader :last_request
 
       alias_method :response, :last_response
-      alias_method :request, :last_request
 
       def initialize(app)
         @app = app
       end
 
-      def get(path, data=nil, headers=nil)
-        request!("GET", path, data, headers)
+      def get(path, data=nil, headers={})
+        request(path, data, headers.merge(:verb => "GET"))
       end
 
-      def post(path, data=nil, headers=nil)
-        request!("POST", path, data, headers)
+      def post(path, data=nil, headers={})
+        request(path, data, headers.merge(:verb => "POST"))
       end
 
-      private
-        def request!(verb, path, data=nil, headers=nil)
-          env = env_for(verb, path, data, headers)
-          @last_request  = Rack::Request.new(env)
-          @last_response = Rack::Response.new(@app.call(env))
-        end
+      def request(path, data=nil, headers={})
+        env = env_for(path, data, headers)
+        
+        @last_request  = Rack::Request.new(env)
+        @last_response = Rack::Response.new(@app.call(env))
+      end
+      
+    private
 
-        def env_for(verb, path, data, headers)
-          uri = URI(path)
-          uri.query = param_string(data) if data.is_a?(Hash)
-          options = { :method => verb }
-          options.merge!(headers) if headers
+      def env_for(path, data, headers)
+        verb = headers.delete(:verb)
+        
+        uri = URI(path)
+        uri.query = param_string(data) if data.is_a?(Hash)
+        options = { :method => verb }
+        options.merge!(headers) if headers
 
-          if data.is_a?(Hash)
-            env = data.delete(:headers)
+        if data.is_a?(Hash)
+          env = data.delete(:headers)
 
-            if verb == "POST"
-              options["Content-Type"] = "application/x-www-form-urlencoded"
-              options[:input] = param_string(data)
-            end
-          end
-
-          env = headers.delete(:headers) if headers.is_a?(Hash)
-          options[:input] ||= data       if data.is_a?(String)
-          options.merge!(env)            if env
-
-          Rack::MockRequest.env_for(uri.to_s, options)
-        end
-
-        def param_string(value, prefix = nil)
-          case value
-          when Array
-            value.map { |v|
-              param_string(v, "#{prefix}[]")
-            } * "&"
-          when Hash
-            value.map { |k, v|
-              param_string(v, prefix ? "#{prefix}[#{escape(k)}]" : escape(k))
-            } * "&"
-          else
-            "#{prefix}=#{escape(value)}"
+          if verb == "POST"
+            options["Content-Type"] = "application/x-www-form-urlencoded"
+            options[:input] = param_string(data)
           end
         end
+
+        env = headers.delete(:headers) if headers.is_a?(Hash)
+        options[:input] ||= data       if data.is_a?(String)
+        options.merge!(env)            if env
+
+        Rack::MockRequest.env_for(uri.to_s, options)
+      end
+
+      def param_string(value, prefix = nil)
+        case value
+        when Array
+          value.map { |v|
+            param_string(v, "#{prefix}[]")
+          } * "&"
+        when Hash
+          value.map { |k, v|
+            param_string(v, prefix ? "#{prefix}[#{escape(k)}]" : escape(k))
+          } * "&"
+        else
+          "#{prefix}=#{escape(value)}"
+        end
+      end
+      
     end
   end
 end
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + "/spec_helper"
 
-describe Rack::Test do
+describe Rack::Test::Session do
   before do
     @session = Rack::Test::Session.new(App)
   end
@@ -13,6 +13,59 @@ describe Rack::Test do
     @session.last_response
   end
 
+  describe "#request" do
+    xit "requests the URI" do
+      @session.request "/"
+      response.should be_ok
+    end
+    
+    it "uses the provided env"
+    it "keeps a cookie jar"
+    it "sends multipart requests"
+    it "yields the response to a given block"
+    it "calls callbacks before each request"
+    it "calls callbacks after each request"
+        
+    context "when the URL is https://" do
+      it "sets SERVER_PORT to 443"
+      it "sets HTTPS to on"
+    end
+  end
+  
+  describe "#last_request" do
+    it "returns the most recent request"
+    it "raises an error if no requests have been issued"
+  end
+  
+  describe "#last_response" do
+    it "returns the most recent response"
+    it "raises an error if no requests have been issued"
+  end
+  
+  describe "#get" do
+    it "requests the URL using GET"
+    it "accepts a params hash"
+    it "uses the provided env"
+  end
+  
+  describe "#post" do
+    it "requests the URL using GET"
+    it "accepts a params hash"
+    it "uses the provided env"
+  end
+  
+  describe "#put" do
+    it "requests the URL using GET"
+    it "accepts a params hash"
+    it "uses the provided env"
+  end
+  
+  describe "#delete" do
+    it "requests the URL using GET"
+    it "accepts a params hash"
+    it "uses the provided env"
+  end
+  
   describe "#get" do
     it "requests URL" do
       @session.get "/"
@@ -25,7 +78,7 @@ describe Rack::Test do
     end
 
     it "allows to modify the rack env" do
-      @session.get "/", :headers => { "User-Agent" => "Rack::Test" }
+      @session.get "/", {}, :headers => { "User-Agent" => "Rack::Test" }
       request.env["User-Agent"].should == "Rack::Test"
     end
   end
@@ -47,7 +100,7 @@ describe Rack::Test do
     end
 
     it "allows to specify headers" do
-      @session.post "/", :headers => { "X-Foo" => "bar" }
+      @session.post "/", {}, :headers => { "X-Foo" => "bar" }
       request.env["X-Foo"].should == "bar"
     end
 
