e7ccd0dece4a3c21a21f47faef64d4cc4d0737a0
Bryan Helmkamp <bryan@brynary.com>
2009-09-19 17:16:20 -0400

    Prep for 0.5.0 release

--- lib/rack/test.rb
+++ lib/rack/test.rb
@@ -9,7 +9,7 @@ require "rack/test/uploaded_file"
 
 module Rack
   module Test
-    VERSION = "0.4.3pre"
+    VERSION = "0.5.0"
 
     DEFAULT_HOST = "example.org"
     MULTIPART_BOUNDARY = "----------XnJLe9ZIbbGUYtzPQJ16u1"
