3081bc2f0631a99ded064b20d34cbe3eaeb9a8a4
Bryan Helmkamp <bryan@brynary.com>
2009-11-27 16:39:40 -0500

    Ruby 1.8.6 compat: Don't rely on String#start_with?

--- lib/rack/test/cookie_jar.rb
+++ lib/rack/test/cookie_jar.rb
@@ -71,7 +71,7 @@ module Rack
           uri.host = @default_host
         end
 
-        real_domain = domain.start_with?('.') ? domain[1..-1] : domain
+        real_domain = domain =~ /^\./ ? domain[1..-1] : domain
         (!secure? || (secure? && uri.scheme == "https")) &&
         uri.host =~ Regexp.new("#{Regexp.escape(real_domain)}$", Regexp::IGNORECASE) &&
         uri.path =~ Regexp.new("^#{Regexp.escape(path)}")
