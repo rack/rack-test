7bebfb3e769decdaa85a36026419195b72a58940
Bryan Helmkamp <bryan@brynary.com>
2010-09-13 08:34:40 -0400

    Whitespace

--- spec/support/matchers/body.rb
+++ spec/support/matchers/body.rb
@@ -2,7 +2,7 @@ RSpec::Matchers.define :have_body do |expected|
   match do |response|
     response.body.should == expected
   end
-  
+
   description do
     "have body #{expected.inspect}"
   end
--- spec/support/matchers/challenge.rb
+++ spec/support/matchers/challenge.rb
@@ -4,7 +4,7 @@ RSpec::Matchers.define :be_challenge do
     actual_response['WWW-Authenticate'] =~ /^Digest / &&
     actual_response.body.empty?
   end
-  
+
   description do
     "a HTTP Digest challenge response"
   end
