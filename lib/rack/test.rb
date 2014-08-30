3c09564749aeb23d63e0de61043f7a465d555863
Johannes Barre <igel@igels.net>
2014-08-30 22:39:49 +0200

    Introduce custom_request to support all verbs
    I believe, it's not useful to support every HTTP verb defined
    everywhere with a custom method. But it should be possible to use those
    verbs, thus we should introduce a method to trigger requests with a
    custom verb. Unfortunately, there is already a public request method, so
    I had to choose a different name.

--- lib/rack/test.rb
+++ lib/rack/test.rb
@@ -55,8 +55,7 @@ module Rack
       # Example:
       #   get "/"
       def get(uri, params = {}, env = {}, &block)
-        env = env_for(uri, env.merge(method: 'GET', params: params))
-        process_request(uri, env, &block)
+        custom_request('GET', uri, params, env, &block)
       end
 
       # Issue a POST request for the given URI. See #get
@@ -64,8 +63,7 @@ module Rack
       # Example:
       #   post "/signup", "name" => "Bryan"
       def post(uri, params = {}, env = {}, &block)
-        env = env_for(uri, env.merge(method: 'POST', params: params))
-        process_request(uri, env, &block)
+        custom_request('POST', uri, params, env, &block)
       end
 
       # Issue a PUT request for the given URI. See #get
@@ -73,8 +71,7 @@ module Rack
       # Example:
       #   put "/"
       def put(uri, params = {}, env = {}, &block)
-        env = env_for(uri, env.merge(method: 'PUT', params: params))
-        process_request(uri, env, &block)
+        custom_request('PUT', uri, params, env, &block)
       end
 
       # Issue a PATCH request for the given URI. See #get
@@ -82,8 +79,7 @@ module Rack
       # Example:
       #   patch "/"
       def patch(uri, params = {}, env = {}, &block)
-        env = env_for(uri, env.merge(method: 'PATCH', params: params))
-        process_request(uri, env, &block)
+        custom_request('PATCH', uri, params, env, &block)
       end
 
       # Issue a DELETE request for the given URI. See #get
@@ -91,8 +87,7 @@ module Rack
       # Example:
       #   delete "/"
       def delete(uri, params = {}, env = {}, &block)
-        env = env_for(uri, env.merge(method: 'DELETE', params: params))
-        process_request(uri, env, &block)
+        custom_request('DELETE', uri, params, env, &block)
       end
 
       # Issue an OPTIONS request for the given URI. See #get
@@ -100,8 +95,7 @@ module Rack
       # Example:
       #   options "/"
       def options(uri, params = {}, env = {}, &block)
-        env = env_for(uri, env.merge(method: 'OPTIONS', params: params))
-        process_request(uri, env, &block)
+        custom_request('OPTIONS', uri, params, env, &block)
       end
 
       # Issue a HEAD request for the given URI. See #get
@@ -109,8 +103,7 @@ module Rack
       # Example:
       #   head "/"
       def head(uri, params = {}, env = {}, &block)
-        env = env_for(uri, env.merge(method: 'HEAD', params: params))
-        process_request(uri, env, &block)
+        custom_request('HEAD', uri, params, env, &block)
       end
 
       # Issue a request to the Rack app for the given URI and optional Rack
@@ -122,6 +115,15 @@ module Rack
       #   request "/"
       def request(uri, env = {}, &block)
         env = env_for(uri, env)
+        process_request(uri, env, &block)
+      end
+
+      # Issue a request using the given verb for the given URI. See #get
+      #
+      # Example:
+      #   custom_request "LINK", "/"
+      def custom_request(verb, uri, params = {}, env = {}, &block)
+        env = env_for(uri, env.merge(method: verb.to_s.upcase, params: params))
         process_request(uri, env, &block)
       end
 
