2995e1511b393ed65ea3b334f97a8f8005633d5c
Dennis Sivia <dennis.sivia@metrigo.de>
2016-02-29 16:37:03 +0100

    Update to latest rspec version and syntax.
    In order to make it easier to merge other pull requests,
    we should update to rspec 3.
    This PR updats the Gemfile.lock to use rspec 3.4, it also
    updats the test syntax to match the new rspec testing style.
    
    This Change has been ported from
    [this contribution](https://github.com/brynary/rack-test/pull/134/commits)

--- spec/rack/test/cookie_spec.rb
+++ spec/rack/test/cookie_spec.rb
@@ -5,90 +5,88 @@ describe Rack::Test::Session do
   context "cookies" do
     it "keeps a cookie jar" do
       get "/cookies/show"
-      check last_request.cookies.should == {}
+      check expect(last_request.cookies).to eq({})
 
       get "/cookies/set", "value" => "1"
       get "/cookies/show"
-      last_request.cookies.should == { "value" => "1" }
+      expect(last_request.cookies).to eq({ "value" => "1" })
     end
 
     it "doesn't send expired cookies" do
       get "/cookies/set", "value" => "1"
       now = Time.now
-      Time.stub!(:now => now + 60)
+      allow(Time).to receive_messages(:now => now + 60)
       get "/cookies/show"
-      last_request.cookies.should == {}
+      expect(last_request.cookies).to eq({})
     end
 
     it "cookie path defaults to the uri of the document that was requested" do
-      pending "See issue rack-test github issue #50" do
-        post "/cookies/default-path", "value" => "cookie"
-        get "/cookies/default-path"
-        check last_request.cookies.should == { "simple"=>"cookie" }
-        get "/cookies/show"
-        check last_request.cookies.should == { }
-      end
+      skip "See issue rack-test github issue #50"
+      post "/cookies/default-path", "value" => "cookie"
+      get "/cookies/default-path"
+      expect(last_request.cookies).to eq({ "simple"=>"cookie" })
+      get "/cookies/show"
+      expect(last_request.cookies).to eq({})
     end
 
     it "escapes cookie values" do
       jar = Rack::Test::CookieJar.new
       jar["value"] = "foo;abc"
-      jar["value"].should == "foo;abc"
+      expect(jar["value"]).to eq("foo;abc")
     end
 
     it "deletes cookies directly from the CookieJar" do
       jar = Rack::Test::CookieJar.new
       jar["abcd"] = "1234"
-      jar["abcd"].should == "1234"
+      expect(jar["abcd"]).to eq("1234")
       jar.delete("abcd")
-      jar["abcd"].should == nil
+      expect(jar["abcd"]).to eq(nil)
     end
 
     it "doesn't send cookies with the wrong domain" do
       get "http://www.example.com/cookies/set", "value" => "1"
       get "http://www.other.example/cookies/show"
-      last_request.cookies.should == {}
+      expect(last_request.cookies).to eq({})
     end
 
     it "doesn't send cookies with the wrong path" do
       get "/cookies/set", "value" => "1"
       get "/not-cookies/show"
-      last_request.cookies.should == {}
+      expect(last_request.cookies).to eq({})
     end
 
     it "persists cookies across requests that don't return any cookie headers" do
       get "/cookies/set", "value" => "1"
       get "/void"
       get "/cookies/show"
-      last_request.cookies.should == { "value" => "1" }
+      expect(last_request.cookies).to eq({ "value" => "1" })
     end
 
     it "deletes cookies" do
       get "/cookies/set", "value" => "1"
       get "/cookies/delete"
       get "/cookies/show"
-      last_request.cookies.should == { }
+      expect(last_request.cookies).to eq({ })
     end
 
     it "respects cookie domains when no domain is explicitly set" do
-      pending "FIXME: www.example.org should not get the first cookie" do
-        request("http://example.org/cookies/count").should     have_body("1")
-        request("http://www.example.org/cookies/count").should have_body("1")
-        request("http://example.org/cookies/count").should     have_body("2")
-        request("http://www.example.org/cookies/count").should have_body("2")
-      end
+      skip "FIXME: www.example.org should not get the first cookie"
+      expect(request("http://example.org/cookies/count")).to     have_body("1")
+      expect(request("http://www.example.org/cookies/count")).to have_body("1")
+      expect(request("http://example.org/cookies/count")).to     have_body("2")
+      expect(request("http://www.example.org/cookies/count")).to have_body("2")
     end
 
     it "treats domains case insensitively" do
       get "http://example.com/cookies/set", "value" => "1"
       get "http://EXAMPLE.COM/cookies/show"
-      last_request.cookies.should == { "value" => "1" }
+      expect(last_request.cookies).to eq({ "value" => "1" })
     end
 
     it "treats paths case sensitively" do
       get "/cookies/set", "value" => "1"
       get "/COOKIES/show"
-      last_request.cookies.should == {}
+      expect(last_request.cookies).to eq({})
     end
 
     it "prefers more specific cookies" do
@@ -96,124 +94,124 @@ describe Rack::Test::Session do
       get "http://sub.example.com/cookies/set", "value" => "sub"
 
       get "http://sub.example.com/cookies/show"
-      check last_request.cookies.should == { "value" => "sub" }
+      check expect(last_request.cookies).to eq({ "value" => "sub" })
 
       get "http://example.com/cookies/show"
-      last_request.cookies.should == { "value" => "domain" }
+      expect(last_request.cookies).to eq({ "value" => "domain" })
     end
 
     it "treats cookie names case insensitively" do
       get "/cookies/set", "value" => "lowercase"
       get "/cookies/set-uppercase", "value" => "UPPERCASE"
       get "/cookies/show"
-      last_request.cookies.should == { "VALUE" => "UPPERCASE" }
+      expect(last_request.cookies).to eq({ "VALUE" => "UPPERCASE" })
     end
 
     it "defaults the domain to the request domain" do
       get "http://example.com/cookies/set-simple", "value" => "cookie"
       get "http://example.com/cookies/show"
-      check last_request.cookies.should == { "simple" => "cookie" }
+      check expect(last_request.cookies).to eq({ "simple" => "cookie" })
 
       get "http://other.example/cookies/show"
-      last_request.cookies.should == {}
+      expect(last_request.cookies).to eq({})
     end
 
     it "defaults the domain to the request path up to the last slash" do
       get "/cookies/set-simple", "value" => "1"
       get "/not-cookies/show"
-      last_request.cookies.should == {}
+      expect(last_request.cookies).to eq({})
     end
 
     it "supports secure cookies" do
       get "https://example.com/cookies/set-secure", "value" => "set"
       get "http://example.com/cookies/show"
-      check last_request.cookies.should == {}
+      check expect(last_request.cookies).to eq({})
 
       get "https://example.com/cookies/show"
-      last_request.cookies.should == { "secure-cookie" => "set" }
-      rack_mock_session.cookie_jar['secure-cookie'].should == 'set'
+      expect(last_request.cookies).to eq({ "secure-cookie" => "set" })
+      expect(rack_mock_session.cookie_jar['secure-cookie']).to eq('set')
     end
 
     it "keeps separate cookie jars for different domains" do
       get "http://example.com/cookies/set", "value" => "example"
       get "http://example.com/cookies/show"
-      check last_request.cookies.should == { "value" => "example" }
+      check expect(last_request.cookies).to eq({ "value" => "example" })
 
       get "http://other.example/cookies/set", "value" => "other"
       get "http://other.example/cookies/show"
-      check last_request.cookies.should == { "value" => "other" }
+      check expect(last_request.cookies).to eq({ "value" => "other" })
 
       get "http://example.com/cookies/show"
-      last_request.cookies.should == { "value" => "example" }
+      expect(last_request.cookies).to eq({ "value" => "example" })
     end
 
     it "keeps one cookie jar for domain and its subdomains" do
       get "http://example.org/cookies/subdomain"
       get "http://example.org/cookies/subdomain"
-      last_request.cookies.should == { "count" => "1" }
+      expect(last_request.cookies).to eq({ "count" => "1" })
 
       get "http://foo.example.org/cookies/subdomain"
-      last_request.cookies.should == { "count" => "2" }
+      expect(last_request.cookies).to eq({ "count" => "2" })
     end
 
     it "allows cookies to be cleared" do
       get "/cookies/set", "value" => "1"
       clear_cookies
       get "/cookies/show"
-      last_request.cookies.should == {}
+      expect(last_request.cookies).to eq({})
     end
 
     it "allow cookies to be set" do
       set_cookie "value=10"
       get "/cookies/show"
-      last_request.cookies.should == { "value" => "10" }
+      expect(last_request.cookies).to eq({ "value" => "10" })
     end
 
     it "allows an array of cookies to be set" do
       set_cookie ["value=10", "foo=bar"]
       get "/cookies/show"
-      last_request.cookies.should == { "value" => "10", "foo" => "bar" }
+      expect(last_request.cookies).to eq({ "value" => "10", "foo" => "bar" })
     end
 
     it "skips emtpy string cookies" do
       set_cookie "value=10\n\nfoo=bar"
       get "/cookies/show"
-      last_request.cookies.should == { "value" => "10", "foo" => "bar" }
+      expect(last_request.cookies).to eq({ "value" => "10", "foo" => "bar" })
     end
 
     it "parses multiple cookies properly" do
       get "/cookies/set-multiple"
       get "/cookies/show"
-      last_request.cookies.should == { "key1" => "value1", "key2" => "value2" }
+      expect(last_request.cookies).to eq({ "key1" => "value1", "key2" => "value2" })
     end
 
     it "supports multiple sessions" do
       with_session(:first) do
         get "/cookies/set", "value" => "1"
         get "/cookies/show"
-        last_request.cookies.should == { "value" => "1" }
+        expect(last_request.cookies).to eq({ "value" => "1" })
       end
 
       with_session(:second) do
         get "/cookies/show"
-        last_request.cookies.should == { }
+        expect(last_request.cookies).to eq({ })
       end
     end
 
     it "uses :default as the default session name" do
       get "/cookies/set", "value" => "1"
       get "/cookies/show"
-      check last_request.cookies.should == { "value" => "1" }
+      check expect(last_request.cookies).to eq({ "value" => "1" })
 
       with_session(:default) do
         get "/cookies/show"
-        last_request.cookies.should == { "value" => "1" }
+        expect(last_request.cookies).to eq({ "value" => "1" })
       end
     end
 
     it "accepts explicitly provided cookies" do
       request "/cookies/show", :cookie => "value=1"
-      last_request.cookies.should == { "value" => "1" }
+      expect(last_request.cookies).to eq({ "value" => "1" })
     end
   end
 end
--- spec/rack/test/utils_spec.rb
+++ spec/rack/test/utils_spec.rb
@@ -5,48 +5,48 @@ describe Rack::Test::Utils do
 
   describe "build_nested_query" do
     it "converts empty strings to =" do
-      build_nested_query("").should == "="
+      expect(build_nested_query("")).to eq("=")
     end
 
     it "converts nil to an empty string" do
-      build_nested_query(nil).should == ""
+      expect(build_nested_query(nil)).to eq("")
     end
 
     it "converts hashes with nil values" do
-      build_nested_query(:a => nil).should == "a"
+      expect(build_nested_query(:a => nil)).to eq("a")
     end
 
     it "converts hashes" do
-      build_nested_query(:a => 1).should == "a=1"
+      expect(build_nested_query(:a => 1)).to eq("a=1")
     end
 
     it "converts hashes with multiple keys" do
       hash = { :a => 1, :b => 2 }
-      ["a=1&b=2", "b=2&a=1"].should include(build_nested_query(hash))
+      expect(["a=1&b=2", "b=2&a=1"]).to include(build_nested_query(hash))
     end
 
     it "converts arrays with one element" do
-      build_nested_query(:a => [1]).should == "a[]=1"
+      expect(build_nested_query(:a => [1])).to eq("a[]=1")
     end
 
     it "converts arrays with multiple elements" do
-      build_nested_query(:a => [1, 2]).should == "a[]=1&a[]=2"
+      expect(build_nested_query(:a => [1, 2])).to eq("a[]=1&a[]=2")
     end
 
     it "converts arrays with brackets '[]' in the name" do
-      build_nested_query("a[]" => [1, 2]).should == "a%5B%5D=1&a%5B%5D=2"
+      expect(build_nested_query("a[]" => [1, 2])).to eq("a%5B%5D=1&a%5B%5D=2")
     end
 
     it "converts nested hashes" do
-      build_nested_query(:a => { :b => 1 }).should == "a[b]=1"
+      expect(build_nested_query(:a => { :b => 1 })).to eq("a[b]=1")
     end
 
     it "converts arrays nested in a hash" do
-      build_nested_query(:a => { :b => [1, 2] }).should == "a[b][]=1&a[b][]=2"
+      expect(build_nested_query(:a => { :b => [1, 2] })).to eq("a[b][]=1&a[b][]=2")
     end
 
     it "converts arrays of hashes" do
-      build_nested_query(:a => [{ :b => 2}, { :c => 3}]).should == "a[][b]=2&a[][c]=3"
+      expect(build_nested_query(:a => [{ :b => 2}, { :c => 3}])).to eq("a[][b]=2&a[][c]=3")
     end
   end
 
@@ -62,9 +62,9 @@ describe Rack::Test::Utils do
       }
       env = Rack::MockRequest.env_for("/", options)
       params = Rack::Utils::Multipart.parse_multipart(env)
-      check params["submit-name"].should == "Larry"
-      check params["files"][:filename].should == "foo.txt"
-      params["files"][:tempfile].read.should == "bar\n"
+      check expect(params["submit-name"]).to eq("Larry")
+      check expect(params["files"][:filename]).to eq("foo.txt")
+      expect(params["files"][:tempfile].read).to eq("bar\n")
     end
 
    it "builds multipart bodies from array of files" do
@@ -78,13 +78,13 @@ describe Rack::Test::Utils do
       }
       env = Rack::MockRequest.env_for("/", options)
       params = Rack::Utils::Multipart.parse_multipart(env)
-      check params["submit-name"].should == "Larry"
+      check expect(params["submit-name"]).to eq("Larry")
 
-      check params["files"][0][:filename].should == "foo.txt"
-      params["files"][0][:tempfile].read.should == "bar\n"
+      check expect(params["files"][0][:filename]).to eq("foo.txt")
+      expect(params["files"][0][:tempfile].read).to eq("bar\n")
 
-      check params["files"][1][:filename].should == "bar.txt"
-      params["files"][1][:tempfile].read.should == "baz\n"
+      check expect(params["files"][1][:filename]).to eq("bar.txt")
+      expect(params["files"][1][:tempfile].read).to eq("baz\n")
     end
 
     it "builds nested multipart bodies" do
@@ -98,10 +98,10 @@ describe Rack::Test::Utils do
       }
       env = Rack::MockRequest.env_for("/", options)
       params = Rack::Utils::Multipart.parse_multipart(env)
-      check params["people"][0]["submit-name"].should == "Larry"
-      check params["people"][0]["files"][:filename].should == "foo.txt"
-      params["people"][0]["files"][:tempfile].read.should == "bar\n"
-      check params["foo"].should == ["1", "2"]
+      check expect(params["people"][0]["submit-name"]).to eq("Larry")
+      check expect(params["people"][0]["files"][:filename]).to eq("foo.txt")
+      expect(params["people"][0]["files"][:tempfile].read).to eq("bar\n")
+      check expect(params["foo"]).to eq(["1", "2"])
     end
 
     it "builds nested multipart bodies with an array of hashes" do
@@ -115,9 +115,9 @@ describe Rack::Test::Utils do
       }
       env = Rack::MockRequest.env_for("/", options)
       params = Rack::Utils::Multipart.parse_multipart(env)
-      check params["files"][:filename].should == "foo.txt"
-      params["files"][:tempfile].read.should == "bar\n"
-      check params["foo"].should == [{"id" => "1", "name" => "Dave"}, {"id" => "2", "name" => "Steve"}]
+      check expect(params["files"][:filename]).to eq("foo.txt")
+      expect(params["files"][:tempfile].read).to eq("bar\n")
+      check expect(params["foo"]).to eq([{"id" => "1", "name" => "Dave"}, {"id" => "2", "name" => "Steve"}])
     end
 
     it "builds nested multipart bodies with arbitrarily nested array of hashes" do
@@ -133,11 +133,11 @@ describe Rack::Test::Utils do
       }
       env = Rack::MockRequest.env_for("/", options)
       params = Rack::Utils::Multipart.parse_multipart(env)
-      check params["files"][:filename].should == "foo.txt"
-      params["files"][:tempfile].read.should == "bar\n"
-      check params["foo"].should == {"bar" => [{"id" => "1", "name" => "Dave"},
+      check expect(params["files"][:filename]).to eq("foo.txt")
+      expect(params["files"][:tempfile].read).to eq("bar\n")
+      check expect(params["foo"]).to eq({"bar" => [{"id" => "1", "name" => "Dave"},
                                                {"id" => "2", "name" => "Steve", "qux" => [{"id" => '3', "name" => 'mike'},
-                                                                                          {"id" => '4', "name" => 'Joan'}]}]}
+                                                                                          {"id" => '4', "name" => 'Joan'}]}]})
     end
 
     it 'does not break with params that look nested, but are not' do
@@ -151,10 +151,10 @@ describe Rack::Test::Utils do
       }
       env = Rack::MockRequest.env_for("/", options)
       params = Rack::Utils::Multipart.parse_multipart(env)
-      check params["files"][0][:filename].should == "foo.txt"
-      params["files"][0][:tempfile].read.should == "bar\n"
-      check params["foo"][0].should == "1"
-      check params["bar"][0].should == {"qux" => "2"}
+      check expect(params["files"][0][:filename]).to eq("foo.txt")
+      expect(params["files"][0][:tempfile].read).to eq("bar\n")
+      check expect(params["foo"][0]).to eq("1")
+      check expect(params["bar"][0]).to eq({"qux" => "2"})
     end
 
     it 'allows for nested files' do
@@ -169,21 +169,21 @@ describe Rack::Test::Utils do
       }
       env = Rack::MockRequest.env_for("/", options)
       params = Rack::Utils::Multipart.parse_multipart(env)
-      check params["foo"][0]["id"].should == "1"
-      check params["foo"][0]["data"][:filename].should == "foo.txt"
-      params["foo"][0]["data"][:tempfile].read.should == "bar\n"
-      check params["foo"][1].should == {"id" => "2", "data" => ["3", "4"]}
+      check expect(params["foo"][0]["id"]).to eq("1")
+      check expect(params["foo"][0]["data"][:filename]).to eq("foo.txt")
+      expect(params["foo"][0]["data"][:tempfile].read).to eq("bar\n")
+      check expect(params["foo"][1]).to eq({"id" => "2", "data" => ["3", "4"]})
     end
 
     it "returns nil if no UploadedFiles were used" do
       data = build_multipart("people" => [{"submit-name" => "Larry", "files" => "contents"}])
-      data.should be_nil
+      expect(data).to be_nil
     end
 
     it "raises ArgumentErrors if params is not a Hash" do
-      lambda {
+      expect {
         build_multipart("foo=bar")
-      }.should raise_error(ArgumentError, "value must be a Hash")
+      }.to raise_error(ArgumentError, "value must be a Hash")
     end
 
     def multipart_file(name)
