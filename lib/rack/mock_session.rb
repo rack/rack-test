53fe6334afee0d981e37d1022d118ea374dda40d
Ben Fritsch <beanie@benle.de>
2021-01-04 14:36:47 +0100

    :bow: rubocop

--- lib/rack/mock_session.rb
+++ lib/rack/mock_session.rb
@@ -31,7 +31,7 @@ module Rack
       @last_response = MockResponse.new(status, headers, body, env['rack.errors'].flush)
 
       # close() gets called automatically in newer Rack versions.
-      if Rack::RELEASE.start_with?("1")
+      if Rack::RELEASE.start_with?('1')
         body.close if body.respond_to?(:close)
       end
 
