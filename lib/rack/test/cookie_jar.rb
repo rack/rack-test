f1b4be10f5f723ee3d19ffb441dbbd3087319964
Jeremy Evans <code@jeremyevans.net>
2022-05-13 14:48:19 -0700

    Refactor and update documentation for Rack::Test::Cookie{,Jar}
    Switch Cookie#raw to an attr_reader for simplicitly.
    
    Avoid unnecessary hash allocation in many CookieJar methods
    by iterating over @cookies directly, or using newly added
    private method each_cookie_for, which yields cookie that
    match the URI, instead of building a hash and then accessing
    that.
    
    Speed up #for by building a string instead of building a hash,
    calling values to get an array, calling map on that, and then
    going with a string.

--- lib/rack/test/cookie_jar.rb
+++ lib/rack/test/cookie_jar.rb
@@ -3,76 +3,88 @@ require 'time'
 
 module Rack
   module Test
+    # Represents individual cookies in the cookie jar.  This is considered private
+    # API and behavior of this class can change at any time.
     class Cookie # :nodoc:
       include Rack::Utils
 
-      # :api: private
-      attr_reader :name, :value
+      # The name of the cookie, will be a string
+      attr_reader :name
+      
+      # The value of the cookie, will be a string or nil if there is no value.
+      attr_reader :value
 
-      # :api: private
+      # The raw string for the cookie, without options. Will generally be in
+      # name=value format is name and value are provided.
+      attr_reader :raw
+
       def initialize(raw, uri = nil, default_host = DEFAULT_HOST)
         @default_host = default_host
         uri ||= default_uri
 
         # separate the name / value pair from the cookie options
-        @name_value_raw, options = raw.split(/[;,] */n, 2)
+        @raw, options = raw.split(/[;,] */n, 2)
 
-        @name, @value = parse_query(@name_value_raw, ';').to_a.first
+        @name, @value = parse_query(@raw, ';').to_a.first
         @options = parse_query(options, ';')
 
         if @options['domain']
           @exact_domain_match = false
         else
+          # If the domain attribute is not present in the cookie,
+          # the domain must match exactly.
           @exact_domain_match = true
           @options['domain'] = (uri.host || default_host)
         end
 
-        @options['path']    ||= uri.path.sub(/\/[^\/]*\Z/, '')
+        # Set the path for the cookie to the directory containing
+        # the request if it isn't set.
+        @options['path'] ||= uri.path.sub(/\/[^\/]*\Z/, '')
       end
 
+      # Wether the given cookie can replace the current cookie in the cookie jar.
       def replaces?(other)
         [name.downcase, domain, path] == [other.name.downcase, other.domain, other.path]
       end
 
-      # :api: private
-      def raw
-        @name_value_raw
-      end
-
-      # :api: private
+      # Whether the cookie has a value.
       def empty?
         @value.nil? || @value.empty?
       end
 
-      # :api: private
+      # The explicit or implicit domain for the cookie.
       def domain
         @options['domain']
       end
 
+      # Whether the cookie has the secure flag, indicating it can only be sent over
+      # an encrypted connection.
       def secure?
         @options.key?('secure')
       end
 
+      # Whether the cookie has the httponly flag, indicating it is not available via
+      # a javascript API.
       def http_only?
         @options.key?('HttpOnly') || @options.key?('httponly')
       end
 
-      # :api: private
+      # The explicit or implicit path for the cookie.
       def path
         ([*@options['path']].first.split(',').first || '/').strip
       end
 
-      # :api: private
+      # A Time value for when the cookie expires, if the expires option is set.
       def expires
         Time.parse(@options['expires']) if @options['expires']
       end
 
-      # :api: private
+      # Whether the cookie is currently expired.
       def expired?
         expires && expires < Time.now
       end
 
-      # :api: private
+      # Whether the cookie is valid for the given URI.
       def valid?(uri)
         uri ||= default_uri
 
@@ -84,17 +96,17 @@ module Rack
           uri.path =~ Regexp.new("^#{Regexp.escape(path)}"))
       end
 
-      # :api: private
+      # Cookies that do not match the URI will not be sent in requests to the URI.
       def matches?(uri)
         !expired? && valid?(uri)
       end
 
-      # :api: private
+      # Order cookies by name, path, and domain.
       def <=>(other)
-        # Orders the cookies from least specific to most
         [name, path, domain.reverse] <=> [other.name, other.path, other.domain.reverse]
       end
 
+      # A hash of cookie options, including the cookie value, but excluding the cookie name.
       def to_h
         @options.merge(
           'value'    => @value,
@@ -104,43 +116,62 @@ module Rack
       end
       alias to_hash to_h
 
-      protected
+      private
 
+      # The default URI to use for the cookie, including just the host.
       def default_uri
         URI.parse('//' + @default_host + '/')
       end
     end
 
+    # Represents all cookies for a session, handling adding and
+    # removing cookies, and finding which cookies apply to a given
+    # request.  This is considered private API and behavior of this
+    # class can change at any time.
     class CookieJar # :nodoc:
       DELIMITER = '; '.freeze
 
-      # :api: private
       def initialize(cookies = [], default_host = DEFAULT_HOST)
         @default_host = default_host
-        @cookies = cookies
-        @cookies.sort!
+        @cookies = cookies.sort!
       end
 
+      # Return the value for first cookie with the given name, or nil
+      # if no such cookie exists.
       def [](name)
-        cookies = hash_for(nil)
-        # TODO: Should be case insensitive
-        cookies[name.to_s] && cookies[name.to_s].value
+        name = name.to_s
+        @cookies.each do |cookie|
+          return cookie.value if cookie.name == name
+        end
+        nil
       end
 
+      # Set a cookie with the given name and value in the
+      # cookie jar.
       def []=(name, value)
         merge("#{name}=#{Rack::Utils.escape(value)}")
       end
 
+      # Return the first cookie with the given name, or nil if
+      # no such cookie exists.
       def get_cookie(name)
-        hash_for(nil).fetch(name, nil)
+        @cookies.each do |cookie|
+          return cookie if cookie.name == name
+        end
+        nil
       end
 
+      # Delete all cookies with the given name from the cookie jar.
       def delete(name)
         @cookies.reject! do |cookie|
           cookie.name == name
         end
+        nil
       end
 
+      # Add a string of raw cookie information to the cookie jar,
+      # if the cookie is valid for the given URI.
+      # Cookies should be separated with a newline.
       def merge(raw_cookies, uri = nil)
         return unless raw_cookies
 
@@ -155,6 +186,7 @@ module Rack
         end
       end
 
+      # Add a Cookie to the cookie jar.
       def <<(new_cookie)
         @cookies.reject! do |existing_cookie|
           new_cookie.replaces?(existing_cookie)
@@ -164,36 +196,48 @@ module Rack
         @cookies.sort!
       end
 
-      # :api: private
+      # Return a raw cookie string for the cookie header to
+      # use for the given URI.
       def for(uri)
-        hash_for(uri).values.map(&:raw).join(DELIMITER)
+        buf = String.new
+        delimiter = nil
+
+        each_cookie_for(uri) do |cookie|
+          if delimiter
+            buf << delimiter
+          else
+            delimiter = DELIMITER
+          end
+          buf << cookie.raw
+        end
+
+        buf
       end
 
+      # Return a hash cookie names and cookie values for cookies in the jar.
       def to_hash
         cookies = {}
 
-        hash_for(nil).each do |name, cookie|
-          cookies[name] = cookie.value
+        @cookies.each do |cookie|
+          cookies[cookie.name] = cookie.value
         end
 
         cookies
       end
 
-      protected
+      private
 
-      def hash_for(uri = nil)
-        cookies = {}
-
-        # The cookies are sorted by most specific first. So, we loop through
-        # all the cookies in order and add it to a hash by cookie name if
-        # the cookie can be sent to the current URI. It's added to the hash
-        # so that when we are done, the cookies will be unique by name and
-        # we'll have grabbed the most specific to the URI.
+      # Yield each cookie that matches for the URI.
+      #
+      # The cookies are sorted by most specific first. So, we loop through
+      # all the cookies in order and add it to a hash by cookie name if
+      # the cookie can be sent to the current URI. It's added to the hash
+      # so that when we are done, the cookies will be unique by name and
+      # we'll have grabbed the most specific to the URI.
+      def each_cookie_for(uri)
         @cookies.each do |cookie|
-          cookies[cookie.name] = cookie if !uri || cookie.matches?(uri)
+          yield cookie if !uri || cookie.matches?(uri)
         end
-
-        cookies
       end
     end
   end
--- spec/rack/test/cookie_jar_spec.rb
+++ spec/rack/test/cookie_jar_spec.rb
@@ -4,11 +4,20 @@ describe Rack::Test::CookieJar do
   cookie_value = 'foo;abc'.freeze
   cookie_name = 'a_cookie_name'.freeze
 
+  it '#[] and []= should get and set cookie values' do
+    jar = Rack::Test::CookieJar.new
+    jar[cookie_name].must_be_nil
+    jar[cookie_name] = cookie_value
+    jar[cookie_name].must_equal cookie_value
+    jar[cookie_name+'a'].must_be_nil
+  end
+
   it '#get_cookie with a populated jar returns full cookie objects' do
     jar = Rack::Test::CookieJar.new
+    jar.get_cookie(cookie_name).must_be_nil
     jar[cookie_name] = cookie_value
-    cookie = jar.get_cookie(cookie_name)
-    cookie.must_be_kind_of Rack::Test::Cookie
+    jar.get_cookie(cookie_name).must_be_kind_of Rack::Test::Cookie
+    jar.get_cookie(cookie_name+'a').must_be_nil
   end
 
   it '#for returns the cookie header string delimited by semicolon and a space' do
