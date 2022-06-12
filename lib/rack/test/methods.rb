0b385d309fb92329f4d424aa985e655c17d40b12
Jeremy Evans <code@jeremyevans.net>
2022-06-12 10:07:00 -0700

    Switch to more common frozen string literal magic comment

--- lib/rack/test/methods.rb
+++ lib/rack/test/methods.rb
@@ -1,4 +1,4 @@
-# frozen-string-literal: true
+# frozen_string_literal: true
 
 require 'forwardable'
 
--- lib/rack/test/utils.rb
+++ lib/rack/test/utils.rb
@@ -1,4 +1,4 @@
-# frozen-string-literal: true
+# frozen_string_literal: true
 
 module Rack
   module Test
